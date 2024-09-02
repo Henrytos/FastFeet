b12ce24d6151ecd762abc0db0b5471f9395fc7c7
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-09-02 15:51:46 -0300

    feat: Add email property to Recipient entity

--- src/domain/delivery/enterprise/entites/recipient.ts
+++ src/domain/delivery/enterprise/entites/recipient.ts
@@ -3,11 +3,16 @@ import { UnqiueEntityID } from "@/core/entities/unique-entity-id";
 
 interface RecipientProps {
   name: string;
+  email: string;
 }
 
 export class Recipient extends Entity<RecipientProps> {
   get name(): string {
     return this.name;
+  }
+
+  get email(): string {
+    return this.props.email;
   }
 
   static create(props: RecipientProps, id?: UnqiueEntityID) {
