9465c037c020c29d630071c51bcccdc2410ba0ee
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-09-03 18:23:40 -0300

    feat: create to register , semantic ++=

---
+++ src/domain/delivery/aplication/use-cases/register-delivery-man-by-administrator-use-case.ts
@@ -0,0 +1,54 @@
+import { Either, left, right } from "@/core/either";
+import { AdministratorsRepository } from "../repositories/administrators-repository";
+import { DeliveryMansRepository } from "../repositories/delivery-mans-repository";
+import { AdministratorDoesNotExistError } from "./errors/administrator-does-not-exist-error";
+import { DeliveryMan } from "../../enterprise/entites/delivery-man";
+import { HashGenerator } from "../cryptography/hash-generator";
+import { Cpf } from "../../enterprise/entites/value-object/cpf";
+
+interface RegisterDeliveryManByAdministratorUseCaseRequest {
+  cpf: string;
+  name: string;
+  password: string;
+  administratorId: string;
+}
+type RegisterDeliveryManByAdministratorUseCaseResponse = Either<
+  AdministratorDoesNotExistError,
+  {}
+>;
+
+export class RegisterDeliveryManByAdministratorUseCase {
+  constructor(
+    private administratorsRepository: AdministratorsRepository,
+    private deliveryMansRepository: DeliveryMansRepository,
+    private hashGenerator: HashGenerator
+  ) {}
+
+  async execute({
+    cpf,
+    name,
+    password,
+    administratorId,
+  }: RegisterDeliveryManByAdministratorUseCaseRequest): Promise<RegisterDeliveryManByAdministratorUseCaseResponse> {
+    const administrator = await this.administratorsRepository.findById(
+      administratorId
+    );
+
+    if (!administrator) {
+      return left(new AdministratorDoesNotExistError());
+    }
+
+    const passwordHash = await this.hashGenerator.hash(password);
+
+    const deliveryMan = DeliveryMan.create({
+      cpf: Cpf.createFromValue(cpf),
+      name,
+      password: passwordHash,
+      administratorId: administrator.id,
+    });
+
+    await this.deliveryMansRepository.create(deliveryMan);
+
+    return right({});
+  }
+}
