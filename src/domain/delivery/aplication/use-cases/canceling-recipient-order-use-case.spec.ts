5c173d19bddd059185589212d5b6ba7c2d8a6557
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-09-04 19:59:07 -0300

    feat: caso de uso de cancelar de encomenda pelo adminitrador

---
+++ src/domain/delivery/aplication/use-cases/canceling-recipient-order-use-case.spec.ts
@@ -0,0 +1,116 @@
+import { makeAdministrator } from "@/test/factories/make-administrator";
+import { InMemoryAdministratorsRepository } from "@/test/repositories/in-memory-administrators-repository";
+import { CancelingRecipientOrderUseCase } from "./canceling-recipient-order-use-case";
+import { InMemoryOrdersRepository } from "@/test/repositories/in-memory-orders-repository";
+import { makeOrder } from "@/test/factories/make-order";
+import { AdministratorDoesNotExistError } from "./errors/administrator-does-not-exist-error";
+import { OrderDoesNotExistError } from "./errors/order-does-not-exist-error";
+import { NotAllowedError } from "@/core/errors/not-allowed-error";
+
+describe("cancelling recipient order  use case", () => {
+  let sut: CancelingRecipientOrderUseCase;
+  let inMemoryAdministratorsRepository: InMemoryAdministratorsRepository;
+  let inMemoryOrdersRespository: InMemoryOrdersRepository;
+
+  beforeEach(() => {
+    inMemoryAdministratorsRepository = new InMemoryAdministratorsRepository();
+    inMemoryOrdersRespository = new InMemoryOrdersRepository();
+    sut = new CancelingRecipientOrderUseCase(
+      inMemoryAdministratorsRepository,
+      inMemoryOrdersRespository
+    );
+  });
+
+  it("should be possible to cancel an order", async () => {
+    const administrator = makeAdministrator();
+    inMemoryAdministratorsRepository.items.push(administrator);
+
+    const order = makeOrder({
+      status: "pending",
+    });
+    inMemoryOrdersRespository.items.push(order);
+
+    const result = await sut.execute({
+      administratorId: administrator.id.toString(),
+      orderId: order.id.toString(),
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(inMemoryOrdersRespository.items).toHaveLength(1);
+    expect(inMemoryOrdersRespository.items[0]).toMatchObject({
+      props: {
+        status: "canceled",
+      },
+    });
+  });
+
+  it("should not be possible to cancel an order if there is no assistant", async () => {
+    const administrator = makeAdministrator();
+    inMemoryAdministratorsRepository.items.push(administrator);
+    const order = makeOrder({
+      status: "pending",
+    });
+    inMemoryOrdersRespository.items.push(order);
+
+    const result = await sut.execute({
+      administratorId: "invalid-administrator-id",
+      orderId: order.id.toString(),
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(inMemoryOrdersRespository.items).toHaveLength(1);
+    expect(result.value).toBeInstanceOf(AdministratorDoesNotExistError);
+    expect(inMemoryOrdersRespository.items[0]).toMatchObject({
+      props: {
+        status: "pending",
+      },
+    });
+  });
+
+  it("should not be possible to cancel an order if there is no order", async () => {
+    const administrator = makeAdministrator();
+    inMemoryAdministratorsRepository.items.push(administrator);
+    const order = makeOrder({
+      status: "pending",
+    });
+    inMemoryOrdersRespository.items.push(order);
+
+    const result = await sut.execute({
+      administratorId: administrator.id.toString(),
+      orderId: "invalid-delivery-man-id",
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(inMemoryOrdersRespository.items).toHaveLength(1);
+    expect(result.value).toBeInstanceOf(OrderDoesNotExistError);
+    expect(inMemoryOrdersRespository.items[0]).toMatchObject({
+      props: {
+        status: "pending",
+      },
+    });
+  });
+
+  it("should not be possible to cancel an order if there is no order", async () => {
+    const administrator = makeAdministrator();
+    inMemoryAdministratorsRepository.items.push(administrator);
+    const order = makeOrder({
+      status: "canceled",
+    });
+    inMemoryOrdersRespository.items.push(order);
+
+    const result = await sut.execute({
+      administratorId: administrator.id.toString(),
+      orderId: order.id.toString(),
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(inMemoryOrdersRespository.items).toHaveLength(1);
+    expect(result.value).toBeInstanceOf(NotAllowedError);
+    expect(inMemoryOrdersRespository.items[0]).toMatchObject({
+      props: {
+        status: "canceled",
+        updatedAt: order.updatedAt,
+      },
+    });
+  });
+});
---
+++ src/domain/delivery/aplication/use-cases/canceling-recipient-order-use-case.ts
@@ -0,0 +1,49 @@
+import { Either, left, right } from "@/core/either";
+import { AdministratorsRepository } from "../repositories/administrators-repository";
+import { AdministratorDoesNotExistError } from "./errors/administrator-does-not-exist-error";
+import { OrdersRepository } from "../repositories/orders-repository";
+import { OrderDoesNotExistError } from "./errors/order-does-not-exist-error";
+import { NotAllowedError } from "@/core/errors/not-allowed-error";
+
+interface CancelingRecipientOrderUseCaseRequest {
+  administratorId: string;
+  orderId: string;
+}
+type CancelingRecipientOrderUseCaseResponse = Either<
+  OrderDoesNotExistError | AdministratorDoesNotExistError | NotAllowedError,
+  {}
+>;
+
+export class CancelingRecipientOrderUseCase {
+  constructor(
+    private administratorsRepository: AdministratorsRepository,
+    private orderRespository: OrdersRepository
+  ) {}
+
+  async execute({
+    administratorId,
+    orderId,
+  }: CancelingRecipientOrderUseCaseRequest): Promise<CancelingRecipientOrderUseCaseResponse> {
+    const administrator = await this.administratorsRepository.findById(
+      administratorId
+    );
+
+    if (!administrator) {
+      return left(new AdministratorDoesNotExistError());
+    }
+
+    const order = await this.orderRespository.findById(orderId);
+    if (!order) {
+      return left(new OrderDoesNotExistError());
+    }
+    if (order.status === "canceled") {
+      return left(new NotAllowedError());
+    }
+
+    order.status = "canceled";
+    await this.orderRespository.save(order);
+    //TODO: send notification to recipient1
+
+    return right({});
+  }
+}
