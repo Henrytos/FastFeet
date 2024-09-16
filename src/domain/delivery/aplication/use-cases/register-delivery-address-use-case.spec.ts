b102182c844ca132636ef896d5e1498c13ae1f4c
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-09-15 23:44:06 -0300

    test: register delivery address use case(RED STATE)

---
+++ src/domain/delivery/aplication/use-cases/register-delivery-address-use-case.spec.ts
@@ -0,0 +1,65 @@
+import { makeAdministrator } from "@/test/factories/make-administrator";
+import { RegisterDeliveryAddressUseCase } from "./register-delivery-address-use-case";
+import { InMemoryAdministratorsRepository } from "@/test/repositories/in-memory-administrators-repository";
+import { InMemoryDeliveryAddressRepository } from "@/test/repositories/in-memory-delivery-address-repository";
+import { AdministratorDoesNotExistError } from "./errors/administrator-does-not-exist-error";
+
+describe("register delivery address use case", () => {
+  let sut: RegisterDeliveryAddressUseCase;
+  let inMemoryAdministratorRepository: InMemoryAdministratorsRepository;
+  let inMemoryDeliveryAddressRepository: InMemoryDeliveryAddressRepository;
+
+  beforeEach(() => {
+    inMemoryAdministratorRepository = new InMemoryAdministratorsRepository();
+    inMemoryDeliveryAddressRepository = new InMemoryDeliveryAddressRepository();
+    sut = new RegisterDeliveryAddressUseCase(
+      inMemoryAdministratorRepository,
+      inMemoryDeliveryAddressRepository
+    );
+  });
+
+  it("deveria ser possivel cadastrar endereço", async () => {
+    const administrator = makeAdministrator();
+    inMemoryAdministratorRepository.items.push(administrator);
+
+    const result = await sut.execute({
+      administratorId: administrator.id.toString(),
+      city: "city",
+      latitude: 0,
+      longitude: 0,
+      neighborhood: "neighborhood",
+      number: "number",
+      state: "state",
+      street: "street",
+      zip: "12345678",
+    });
+
+    expect(result.isRight()).toEqual(true);
+    expect(inMemoryDeliveryAddressRepository.items).toHaveLength(1);
+    expect(inMemoryDeliveryAddressRepository.items[0]).toMatchObject({
+      props: {
+        city: "city",
+        latitude: 0,
+        longitude: 0,
+      },
+    });
+  });
+
+  it("não deveria ser possivel cadastrar endereço se não existe um administrator", async () => {
+    const result = await sut.execute({
+      administratorId: "invalid-administrator-id",
+      city: "city",
+      latitude: 0,
+      longitude: 0,
+      neighborhood: "neighborhood",
+      number: "number",
+      state: "state",
+      street: "street",
+      zip: "12345678",
+    });
+
+    expect(result.isLeft()).toEqual(true);
+    expect(inMemoryDeliveryAddressRepository.items).toHaveLength(0);
+    expect(result.value).toBeInstanceOf(AdministratorDoesNotExistError);
+  });
+});
--- src/domain/delivery/aplication/use-cases/register-delivery-address-use-case.ts
+++ src/domain/delivery/aplication/use-cases/register-delivery-address-use-case.ts
@@ -1,9 +1,26 @@
 import { Either, right } from "@/core/either";
+import { AdministratorsRepository } from "../repositories/administrators-repository";
+import { DeliveryAddressRepository } from "../repositories/delivery-address-repository";
 
-interface RegisterDeliveryAddressUseCaseRequest {}
+interface RegisterDeliveryAddressUseCaseRequest {
+  state: string;
+  city: string;
+  neighborhood: string;
+  street: string;
+  zip: string;
+  number: string;
+  latitude: number;
+  longitude: number;
+  administratorId: string;
+}
 
 type RegisterDeliveryAddressUseCaseResponse = Either<null, {}>;
 export class RegisterDeliveryAddressUseCase {
+  constructor(
+    private readonly administratorsRepository: AdministratorsRepository,
+    private readonly deliveryAddressRepository: DeliveryAddressRepository
+  ) {}
+
   async execute({}: RegisterDeliveryAddressUseCaseRequest): Promise<RegisterDeliveryAddressUseCaseResponse> {
     return right({});
   }
