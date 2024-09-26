8a3142970ee93e740744c540e9a28ef7f3b6936b
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-09-25 22:24:34 -0300

    feat:  AdministratorRegistationUseCase

--- src/domain/delivery/aplication/repositories/administrators-repository.ts
+++ src/domain/delivery/aplication/repositories/administrators-repository.ts
@@ -1,7 +1,8 @@
-import { Administrator } from "../../enterprise/entites/administrator";
-import { Cpf } from "../../enterprise/entites/value-object/cpf";
+import { Administrator } from '../../enterprise/entites/administrator';
+import { Cpf } from '../../enterprise/entites/value-object/cpf';
 
 export interface AdministratorsRepository {
   findByCpf(cpf: Cpf): Promise<Administrator | null>;
   findById(id: string): Promise<Administrator | null>;
+  create(administrator: Administrator): Promise<void>;
 }
--- src/domain/delivery/enterprise/entites/value-object/cpf.ts
+++ src/domain/delivery/enterprise/entites/value-object/cpf.ts
@@ -14,15 +14,15 @@ export class Cpf {
   }
 
   static createFromValue(cpf: string) {
-    cpf = cpf.replace(/\D/g, "");
+    cpf = cpf.replace(/\D/g, '');
 
     if (cpf.length !== 11) {
-      throw new Error("O número deve conter exatamente 11 dígitos.");
+      throw new Error('O número deve conter exatamente 11 dígitos.');
     }
 
     const cpfFormat = cpf.replace(
       /(\d{3})(\d{3})(\d{3})(\d{2})/,
-      "$1.$2.$3-$4"
+      '$1.$2.$3-$4',
     );
 
     return new Cpf(cpfFormat);
