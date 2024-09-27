e2335567b498d3d4ea154dbf8e012c024b4b1b64
henry <franzhenry46@gmail.com>
2024-09-27 17:53:11 -0300

    feat:crate prisma service

---
+++ src/infra/database/prisma/prisma.service.ts
@@ -0,0 +1,19 @@
+import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleDestroy, OnModuleInit
+{
+  constructor() {
+    super();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+}
