5c28ce06bf18263d6705be3e3216ea10d2e13152
henry <franzhenry46@gmail.com>
2024-09-29 11:10:19 -0300

    refactor: Rearrange imports in create-user.controller.e2e-spec.ts and create-user.controller.ts

---
+++ src/infra/http/controllers/create-user.controller.e2e-spec.ts
@@ -0,0 +1,5 @@
+describe('2+2 = 4?', () => {
+  it('should', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
--- src/infra/http/controllers/create-user.controller.ts
+++ src/infra/http/controllers/create-user.controller.ts
@@ -1,6 +1,17 @@
 import { AdministratorRegistrationUseCase } from '@/domain/delivery/application/use-cases/administrator-registration-use-case';
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { z } from 'zod';
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
+import { WrongCredentialsError } from '@/domain/delivery/application/use-cases/errors/wrong-credentials-error';
+import { RegisterDeliveryManUseCase } from '@/domain/delivery/application/use-cases/register-delivery-man-use-case';
 
 const createUserBodySchema = z.object({
   name: z.string(),
@@ -14,16 +25,55 @@ type CreateUserBodySchema = z.infer<typeof createUserBodySchema>;
 export class CreateUserController {
   constructor(
     private administratorRegistrationUseCase: AdministratorRegistrationUseCase,
+    private registerDeliveryManUseCase: RegisterDeliveryManUseCase,
   ) {}
 
-  @Post()
-  async handler(@Body() body: CreateUserBodySchema) {
+  @Post('/admin')
+  @HttpCode(201)
+  async handlerCreateAdmin(
+    @Body(new ZodValidationPipe(createUserBodySchema))
+    body: CreateUserBodySchema,
+  ) {
     const { name, cpf, password } = body;
 
-    await this.administratorRegistrationUseCase.execute({
+    const result = await this.administratorRegistrationUseCase.execute({
       name,
       cpf,
       password,
     });
+
+    if (result.isLeft()) {
+      switch (result.value.constructor) {
+        case WrongCredentialsError:
+          throw new ConflictException(result.value.message);
+        default:
+          throw new BadRequestException(result.value.message);
+      }
+    }
+  }
+
+  @Post('/user')
+  @HttpCode(201)
+  async handlerCreateUser(
+    @Body(new ZodValidationPipe(createUserBodySchema))
+    body: CreateUserBodySchema,
+  ) {
+    const { name, cpf, password } = body;
+    const administratorId = '32132';
+    const result = await this.registerDeliveryManUseCase.execute({
+      name,
+      cpf,
+      password,
+      administratorId,
+    });
+
+    if (result.isLeft()) {
+      switch (result.value.constructor) {
+        case WrongCredentialsError:
+          throw new ConflictException(result.value.message);
+        default:
+          throw new BadRequestException(result.value.message);
+      }
+    }
   }
 }
