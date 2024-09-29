a6c0e1b824aa21cd039424266204558a088ee4b4
henry <franzhenry46@gmail.com>
2024-09-29 10:05:48 -0300

    feat:adicionando zodValidationPies para validação mediante zod schemas

---
+++ src/infra/http/pipes/zod-validation-pipe.ts
@@ -0,0 +1,22 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { ZodError, ZodSchema } from 'zod';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    try {
+      const parsedValue = this.schema.transform(value);
+      return parsedValue;
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException(error.message);
+      }
+      throw new Error('Unexpected error occurred while validating input');
+    }
+  }
+}
