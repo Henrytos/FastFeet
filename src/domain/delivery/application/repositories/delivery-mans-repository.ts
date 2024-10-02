2214d9542f1ede19354a257de6ad466903e990a8
henry <franzhenry46@gmail.com>
2024-10-01 21:40:40 -0300

    feat: Add PrismaDeliveryManMapper and PrismaDeliveryMansRepository

--- src/domain/delivery/application/repositories/delivery-mans-repository.ts
+++ src/domain/delivery/application/repositories/delivery-mans-repository.ts
@@ -1,10 +1,10 @@
 import { DeliveryMan } from '@/domain/delivery/enterprise/entities/delivery-man';
 import { Cpf } from '@/domain/delivery/enterprise/entities/value-object/cpf';
 
-export interface DeliveryMansRepository {
-  create(deliveryMan: DeliveryMan): Promise<void>;
-  findById(id: string): Promise<DeliveryMan | null>;
-  findByCpf(cpf: Cpf): Promise<DeliveryMan | null>;
-  save(deliveryMan: DeliveryMan): Promise<void>;
-  delete(deliveryMan: DeliveryMan): Promise<void>;
+export abstract class DeliveryMansRepository {
+  abstract create(deliveryMan: DeliveryMan): Promise<void>;
+  abstract findById(id: string): Promise<DeliveryMan | null>;
+  abstract findByCpf(cpf: Cpf): Promise<DeliveryMan | null>;
+  abstract save(deliveryMan: DeliveryMan): Promise<void>;
+  abstract delete(deliveryMan: DeliveryMan): Promise<void>;
 }
