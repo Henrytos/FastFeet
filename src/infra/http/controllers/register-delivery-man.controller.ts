98b5d076f3cbdb89f99b7c11b26adaafa2e30989
Henry <franzhenry46@gmail.com>
2024-10-28 21:02:55 -0300

    feat: add egister deliveyr controller

---
+++ src/infra/http/controllers/register-delivery-man.controller.ts
@@ -0,0 +1,62 @@
+import { RegisterDeliveryManUseCase } from "@/domain/delivery/application/use-cases/register-delivery-man-use-case";
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { z } from "zod";
+import { ZodValidationPipe } from "../pipes/zod-validation-pipe";
+import { CurrentUser } from "@/infra/auth/current-user";
+import { UserPayload } from "@/infra/auth/jwt.strategy";
+import { Roles } from "../guards/roles.decorator";
+import { AdministratorDoesNotExistError } from "@/domain/delivery/application/use-cases/errors/administrator-does-not-exist-error";
+import { WrongCredentialsError } from "@/domain/delivery/application/use-cases/errors/wrong-credentials-error";
+
+const registerDeliveryManBodySchema = z.object({
+  name: z.string(),
+  cpf: z.string().length(11),
+  password: z.string().min(6).max(20),
+});
+
+type RegisterDeliveryManBody = z.infer<typeof registerDeliveryManBodySchema>;
+
+const validationZodPipe = new ZodValidationPipe(registerDeliveryManBodySchema);
+
+@Controller("/deliveryman")
+export class RegisterDeliveryManController {
+  constructor(
+    private readonly registerDeliveryManUseCase: RegisterDeliveryManUseCase,
+  ) { }
+
+  @Post()
+  @Roles("ADMINISTRATOR")
+  @HttpCode(HttpStatus.CREATED)
+  async handler(
+    @CurrentUser() administrator: UserPayload,
+    @Body(validationZodPipe) body: RegisterDeliveryManBody,
+  ) {
+    const { name, cpf, password } = body;
+
+    const result = await this.registerDeliveryManUseCase.execute({
+      administratorId: administrator.sub,
+      name,
+      cpf,
+      password,
+    });
+
+    if (result.isLeft()) {
+      switch (result.value.constructor) {
+        case AdministratorDoesNotExistError:
+          throw new UnauthorizedException(result.value.message);
+        case WrongCredentialsError:
+          throw new BadRequestException(result.value.message);
+        default:
+          throw new BadRequestException();
+      }
+    }
+  }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,24 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
 
-import { AdministratorRegistrationUseCase } from '@/domain/delivery/application/use-cases/administrator-registration-use-case';
+import { AdministratorRegistrationUseCase } from "@/domain/delivery/application/use-cases/administrator-registration-use-case";
 
-import { CryptographyModule } from '@/infra/cryptography/cryptography.module';
-import { DatabaseModule } from '@/infra/database/database.module';
-import { CreateAccountController } from './controllers/create-account.controller';
-import { RegisterDeliveryManUseCase } from '@/domain/delivery/application/use-cases/register-delivery-man-use-case';
-import { AuthenticateController } from './controllers/authenticate.controller';
-import { AuthenticateAdministratorUseCase } from '@/domain/delivery/application/use-cases/authenticate-administrator-use-case';
-import { AuthenticateDeliveryManUseCase } from '@/domain/delivery/application/use-cases/authenticate-delivery-man-use-case';
-import { GetProfileController } from './controllers/get-profile.controller';
-import { DeleteDeliveryManController } from './controllers/delete-delivery-man.controller';
-import { UpdateDeliveryManController } from './controllers/update-delivery-man.controller';
-import { ChangeDeliveryManPasswordController } from './controllers/change-delivery-man-password.controller';
-import { DeleteDeliveryManByIdUseCase } from '@/domain/delivery/application/use-cases/delete-delivery-man-by-id-use-case';
-import { UpdateDeliveryManByAdministratorUseCase } from '@/domain/delivery/application/use-cases/update-delivery-man-by-administrator';
-import { ChangeDeliveryManPasswordUseCase } from '@/domain/delivery/application/use-cases/change-delivery-man-password-use-case';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuards } from './guards/roles.guards';
-import { GuardsModule } from './guards/guards.module';
+import { CryptographyModule } from "@/infra/cryptography/cryptography.module";
+import { DatabaseModule } from "@/infra/database/database.module";
+import { CreateAccountController } from "./controllers/create-account.controller";
+import { RegisterDeliveryManUseCase } from "@/domain/delivery/application/use-cases/register-delivery-man-use-case";
+import { AuthenticateController } from "./controllers/authenticate.controller";
+import { AuthenticateAdministratorUseCase } from "@/domain/delivery/application/use-cases/authenticate-administrator-use-case";
+import { AuthenticateDeliveryManUseCase } from "@/domain/delivery/application/use-cases/authenticate-delivery-man-use-case";
+import { GetProfileController } from "./controllers/get-profile.controller";
+import { DeleteDeliveryManController } from "./controllers/delete-delivery-man.controller";
+import { UpdateDeliveryManController } from "./controllers/update-delivery-man.controller";
+import { ChangeDeliveryManPasswordController } from "./controllers/change-delivery-man-password.controller";
+import { DeleteDeliveryManByIdUseCase } from "@/domain/delivery/application/use-cases/delete-delivery-man-by-id-use-case";
+import { UpdateDeliveryManByAdministratorUseCase } from "@/domain/delivery/application/use-cases/update-delivery-man-by-administrator";
+import { ChangeDeliveryManPasswordUseCase } from "@/domain/delivery/application/use-cases/change-delivery-man-password-use-case";
+import { GuardsModule } from "./guards/guards.module";
+import { RegisterDeliveryManController } from "./controllers/register-delivery-man.controller";
 
 @Module({
   imports: [DatabaseModule, CryptographyModule, GuardsModule],
@@ -29,6 +28,7 @@ import { GuardsModule } from './guards/guards.module';
     DeleteDeliveryManController,
     UpdateDeliveryManController,
     ChangeDeliveryManPasswordController,
+    RegisterDeliveryManController
   ],
   providers: [
     AdministratorRegistrationUseCase,
@@ -40,4 +40,4 @@ import { GuardsModule } from './guards/guards.module';
     ChangeDeliveryManPasswordUseCase,
   ],
 })
-export class HttpModule {}
+export class HttpModule { }
