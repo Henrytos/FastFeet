a00948455c6fe559e3346bbf562f3dd97b61a3a5
Henry <franzhenry46@gmail.com>
2024-10-28 23:56:35 -0300

    adicionando configuração do swagger para documentação da api

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,9 +1,24 @@
+
+
+
+
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { EnvService } from "./env/env.service";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fastfeet API')
+    .setDescription('API para controle de encomendas de uma transportadora fictícia, a FastFeet.')
+    .setVersion('1.0')
+    .addTag('sessions')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   const envService = app.get(EnvService);
   const PORT = envService.get("PORT");
   await app.listen(PORT);
